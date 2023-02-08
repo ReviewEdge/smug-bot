1b8803cf5d62282d4544006199aa6a1bb94e2a27
ReviewEdge <reviewedge56@gmail.com>
2023-02-08 18:16:35 -0500

    make safer

--- get_verses.py
+++ get_verses.py
@@ -1,11 +1,12 @@
 import json
 import requests
+import config
 
 class BibleVerse():
     def __init__(self, book, chapter, verse):
         self.url = "https://ajith-holy-bible.p.rapidapi.com/GetVerseOfaChapter"
         self.headers = {
-	      "X-RapidAPI-Key": "fb644175d1msh21c23d7949dd64ap1db13djsna91b35e8bd05",
+	      "X-RapidAPI-Key": config.bible_api_key,
 	      "X-RapidAPI-Host": "ajith-holy-bible.p.rapidapi.com"
         }
 
